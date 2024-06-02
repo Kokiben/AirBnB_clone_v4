508eb61a9d218994abd4e5852ecb7131252cb397
Kokiben <bennassarkautar@gmail.com>
2024-06-02 13:52:30 -0700

    API status

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    // Object to keep track of selected amenities
+    let amenities = {};
+
+    // Listen for changes on each input checkbox tag
+    $(document).on('change', "input[type='checkbox']", function () {
+        const amenityId = $(this).data('id');
+        const amenityName = $(this).data('name');
+
+        // If the checkbox is checked, store the Amenity ID in the object
+        if (this.checked) {
+            amenities[amenityId] = amenityName;
+        } else {
+            // If the checkbox is unchecked, remove the Amenity ID from the object
+            delete amenities[amenityId];
+        }
+
+        // Update the h4 tag inside the div Amenities with the list of Amenities checked
+        let amenitiesList = Object.values(amenities);
+        if (amenitiesList.length > 0) {
+            $('div.amenities > h4').text(amenitiesList.join(', '));
+        } else {
+            $('div.amenities > h4').html('&nbsp;');
+        }
+    });
+});
